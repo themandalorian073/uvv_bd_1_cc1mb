1579284bc59131b3b9d6b6c3592b9ed3a1fc0bbb
the_mandalorian73 <amandaarnoni73@protonmail.com>
2022-04-23 19:06:51 -0300

    Update uvv.sql

--- MariaDB/uvv.sql
+++ MariaDB/uvv.sql
@@ -1,13 +1,22 @@
-mysql -u root -p
+create user amandaarnoni@localhost IDENTIFIED by '1234567';
 
-create user 'amandaarnoni'@'localhost';
+create database uvv;
+-- owner = amandaarnoni 
+-- template = template0 
+-- encoding = 'UTF8' 
+-- lc_collate = 'pt_BR.UTF-8' 
+-- lc_ctype = 'pt_BR.UTF-8'
+-- allow_connections = true;
 
-create database `uvv`;
+create schema elmasri;
+
+grant all privileges on uvv.* to amandaarnoni@localhost;
+
+flush privileges;
 
 exit;
 
-create user 'amandaarnoni'@'localhost';
+mysql -u amandaarnoni -p
 
-grant all privileges on uvv.* to amandaarnoni;
 
-mysql -u amandaarnoni
+
