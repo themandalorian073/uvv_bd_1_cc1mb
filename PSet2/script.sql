94e3d25116763a94af8e029fe4ce3828dedf654c
the_mandalorian73 <102439743+themandalorian073@users.noreply.github.com>
2022-04-29 14:12:57 -0300

    Update script.sql

--- PSet2/script.sql
+++ PSet2/script.sql
@@ -24,3 +24,15 @@ where sexo = 'M';
 
 -- Questão 3 --
 
+-- Questão 4 --
+
+select primeiro_nome, nome_meio, ultimo_nome, salario as salario_atual, salario as salario_20, salario as salario_15
+
+
+
+from funcionario;
+
+-- Questão 5 --
+
+
+
