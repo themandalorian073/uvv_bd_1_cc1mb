c6eb0fafec09fd0259f25b44a426f87d55dfeac7
the_mandalorian73 <102439743+themandalorian073@users.noreply.github.com>
2022-04-29 13:30:19 -0300

    Update script.sql

---
+++ PSet2/script.sql
@@ -0,0 +1,15 @@
+-- Questão 1 --
+
+select avg(salario)
+from funcionario
+where numero_departamento = 5;
+
+select avg(salario)
+from funcionario
+where numero_departamento = 4;
+
+select avg(salario)
+from funcionario
+where numero_departamento = 1;
+
+-- Questão 2 --
