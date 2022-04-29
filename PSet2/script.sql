6d73d86caf1cb39f35ad08d194a41883fca16367
the_mandalorian73 <102439743+themandalorian073@users.noreply.github.com>
2022-04-29 13:35:38 -0300

    Update script.sql

--- PSet2/script.sql
+++ PSet2/script.sql
@@ -13,3 +13,14 @@ from funcionario
 where numero_departamento = 1;
 
 -- Questão 2 --
+
+select avg(salario)
+from funcionario
+where sexo = 'F';
+
+select avg(salario)
+from funcionario
+where sexo = 'M';
+
+-- Questão 3 --
+
