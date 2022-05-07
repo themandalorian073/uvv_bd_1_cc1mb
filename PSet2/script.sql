f5f1f16743662a6b2033126305e64f83782e6ae4
the_mandalorian73 <102439743+themandalorian073@users.noreply.github.com>
2022-05-07 17:45:49 -0300

    commit

--- PSet2/script.sql
+++ PSet2/script.sql
@@ -91,7 +91,6 @@ select concat (primeiro_nome, nome_meio, ultimo_nome) as nome_funcionario, numer
 from funcionario
 where exists (select * from dependente where funcionario.cpf != dependente.cpf_funcionario);
 
-
 -- Questão 8 --
 
 -- Questão 9 --
