90092a22da5a941da0b5522701c9d36f5045e6e5
the_mandalorian73 <102439743+themandalorian073@users.noreply.github.com>
2022-06-06 12:32:17 -0300

    commit

---
+++ PSet3/Descrição geral.md
@@ -0,0 +1,11 @@
+## Amanda Arnoni Nascimento
+
+### Professor Abrantes Araujo Silva Filho
+
+#### CC1mB
+
+---
+
+Esta é a descrição geral do que se pode encontrar neste subdiretório.
+
+1.  Arquivo **script.sql** com a resposta do PSet3. Para fazer este trabalho foi utilizado **SQL** no modelo **PostgreSQL**.
--- PSet3/script.sql
+++ PSet3/script.sql
@@ -1,80 +1,15 @@
--- SQL SERVER – Introduction to Hierarchical Query using a Recursive CTE – A Primer --
+WITH recursive expression_name
+AS ( SELECT cod_pai.codigo, cod_pai.nome, cod_pai.codigo_pai
+FROM classificacao as cod_pai
+WHERE cod_pai.codigo_pai IS NULL
 
--- basic sintax of CTE 
-WITH MyCTE
-AS ( SELECT EmpID, FirstName, LastName, ManagerID
-FROM Employee
-WHERE ManagerID IS NULL )
-SELECT *
-FROM MyCTE
-
--- example of recursive CTE
-WITH MyCTE
-AS ( SELECT EmpID, FirstName, LastName, ManagerID
-FROM Employee
-WHERE ManagerID IS NULL
 UNION ALL
-SELECT EmpID, FirstName, LastName, ManagerID
-FROM Employee
-INNER JOIN MyCTE ON Employee.ManagerID = MyCTE.EmpID
-WHERE Employee.ManagerID IS NOT NULL )
-SELECT *
-FROM MyCTE
 
--- adding anchor and recursive quary to a CTE
-WITH EmployeeList
-AS ( SELECT Boss.EmpID, Boss.FirstName, Boss.LastName, Boss.ManagerID
-FROM Employee AS Boss
-WHERE Boss.ManagerID IS NULL
-UNION ALL
-SELECT Emp.EmpID, Emp.FirstName, Emp.LastName, Emp.ManagerID
-FROM Employee AS Emp
-WHERE Employee.ManagerID IS NOT NULL )
-SELECT * FROM EmployeeList
+SELECT cod_filho.codigo, cod_filho.nome, cod_filho.codigo_pai
+FROM classificacao AS cod_filho
 
--- adding expressions to track hierarchical level
-WITH EmployeeList
-AS ( SELECT Boss.EmpID, Boss.FirstName, Boss.LastName, Boss.ManagerID
-1 as EmpLevel
-FROM Employee AS Boss
-WHERE Boss.ManagerID IS NULL
-UNION ALL
-SELECT Emp.EmpID, Emp.FirstName, Emp.LastName, Emp.ManagerID
-2 as EmpLevel -- just as a placeholder
-FROM Employee AS Emp
-WHERE Employee.ManagerID IS NOT NULL )
-SELECT * FROM EmployeeList
+INNER JOIN expression_name AS EL
+ON cod_filho.codigo = EL.codigo
+WHERE cod_filho.codigo IS NOT NULL )
 
--- adding a self-referencing INNER JOIN statement
-WITH EmployeeList
-AS ( SELECT Boss.EmpID, Boss.FirstName, Boss.LastName, Boss.ManagerID
-1 as EmpLevel
-FROM Employee AS Boss
-WHERE Boss.ManagerID IS NULL
-UNION ALL
-SELECT Emp.EmpID, Emp.FirstName, Emp.LastName, Emp.ManagerID, EL.EmpLevel + 1
-FROM Employee AS Emp
-INNER JOIN EmployeeList AS EL
-ON Emp.ManegerID = EL.EmpID
-WHERE Employee.ManagerID IS NOT NULL )
-SELECT * FROM EmployeeList
-
-
-
--- SQL Server Recursive CTE --
--- Summary: in this tutorial, you will learn how to use the SQL Server recursive CTE to query hierarchical data. --
-
--- basic sintax of a recursive CTE
-WITH expression_name (column_list)
-AS
-(
-    -- Anchor member
-    initial_query  
-    UNION ALL
-    -- Recursive member that references expression_name.
-    recursive_query  
-)
--- references expression name
-SELECT *
-FROM   expression_name
-
+SELECT * FROM expression_name;
